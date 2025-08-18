7dd7f503b7e124dff8496865a10437ad775d300c
Nikita Sutulov <notsid212@gmail.com>
2025-08-18 18:44:25 +0300

    fixed the dto to create user
    Added password field to CreateUserDto.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({ example: 'username' })
+  @IsString()
   @IsNotEmpty()
   username: string;
 
   @ApiProperty({ example: '$tr0nG_Passw0rd' })
+  @IsString()
   @IsNotEmpty()
   password: string;
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
-import { OmitType } from '@nestjs/swagger';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { UserDto } from './user.dto';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto extends OmitType(UserDto, [
   'id',
   'role',
   'createdAt',
   'updatedAt',
-] as const) {}
+] as const) {
+  @ApiProperty({ example: '$tr0nG_Passw0rd' })
+  @IsNotEmpty()
+  password: string;
+}
